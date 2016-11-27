4ed5eb8a2941cd9257a3e9afabe91f3bd7f09533
Freddy Hernandez Jr <freddhj@gmail.com>
2016-11-26 22:44:00 -0800

    Initial Commit

---
+++ FractalFriend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FractalFriend
+//
+//  Created by Freddy Hernandez on 11/26/16.
+//  Copyright © 2016 Freddy Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
